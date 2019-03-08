af32a8d18db627a7c787611f34cc7456aa585779
Omar Chbaklo <chbakloomar@gmail.com>
2019-03-08 12:32:45 -0500

    setup

--- TestRun.py
+++ TestRun.py
@@ -5,39 +5,96 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def draw_boxes(image, bounds, color,width=5):
+    draw = ImageDraw.Draw(image)
+    for bound in bounds:
+        draw.line([
+            bound.vertices[0].x, bound.vertices[0].y,
+            bound.vertices[1].x, bound.vertices[1].y,
+            bound.vertices[2].x, bound.vertices[2].y,
+            bound.vertices[3].x, bound.vertices[3].y,
+            bound.vertices[0].x, bound.vertices[0].y],fill=color, width=width)
+    return image
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-<<<<<<< HEAD
-    'IMG_3124.JPG')
-=======
-    'wholesome_pic.png')
->>>>>>> 7bc22369913c81ada3fcc023ad6fea2c2acc540a
+def get_document_bounds(response, feature):
+    for i,page in enumerate(document.pages):
+        for block in page.blocks:
+            if feature==FeatureType.BLOCK:
+                bounds.append(block.bounding_box)
+            for paragraph in block.paragraphs:
+                if feature==FeatureType.PARA:
+                    bounds.append(paragraph.bounding_box)
+                for word in paragraph.words:
+                    for symbol in word.symbols:
+                        if (feature == FeatureType.SYMBOL):
+                            bounds.append(symbol.bounding_box)
+                    if (feature == FeatureType.WORD):
+                        bounds.append(word.bounding_box)
+    return bounds
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+def assemble_word(word):
+    assembled_word=""
+    for symbol in word.symbols:
+        assembled_word+=symbol.text
+    return assembled_word
 
-image = types.Image(content=content)
+def find_word_location(document,word_to_find):
+    for page in document.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    assembled_word=assemble_word(word)
+                    if(assembled_word==word_to_find):
+                        return word.bounding_box
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+def text_within(document,x1,y1,x2,y2): 
+    text=""
+    for page in document.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    for symbol in word.symbols:
+                        min_x=min(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
+                        max_x=max(symbol.bounding_box.vertices[0].x,symbol.bounding_box.vertices[1].x,symbol.bounding_box.vertices[2].x,symbol.bounding_box.vertices[3].x)
+                        min_y=min(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
+                        max_y=max(symbol.bounding_box.vertices[0].y,symbol.bounding_box.vertices[1].y,symbol.bounding_box.vertices[2].y,symbol.bounding_box.vertices[3].y)
+                        if(min_x >= x1 and max_x <= x2 and min_y >= y1 and max_y <= y2):
+                            text+=symbol.text
+                            if(symbol.property.detected_break.type==1 or 
+                                symbol.property.detected_break.type==3):
+                                text+=' '
+                            if(symbol.property.detected_break.type==2):
+                                text+='\t'
+                            if(symbol.property.detected_break.type==5):
+                                text+='\n'
+    return text
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+if __name__ == "__main__":
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
-#find document type method
-def findDocType():
-	print("I am finding doc type!")
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'Alphabet_10K_2017.pdf')
 
-findDocType()
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-#check if table method that returns true or false
-#
-#method-> extract data
-#parameter String-> extraction World
+    image = types.Image(content=content)
+
+    # Performs text detection on the image file
+    response = client.document_text_detection(image=image)
+    document = response.full_text_annotation
+    
+    #print('Text:')
+    #for label in labels:
+    #    print(label.description)
+
+    #bounds=get_document_bounds(response, FeatureType.WORD)
+    #draw_boxes(image,bounds, 'yellow')
+
+    location=find_word_location(document,'net sales')
+    print(document)
+    #text_within(document, location.vertices[1].x, location.vertices[1].y, 30+location.vertices[1].x+(location.vertices[1].x-location.vertices[0].x),location.vertices[2].y)
