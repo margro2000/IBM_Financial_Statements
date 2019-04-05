327c1fa4434125dba223e71fbff5d9296a25d3ca
Margarita Groisman <mg@Margaritas-MacBook-Pro.local>
2019-04-05 18:03:15 -0400

    commented, cleaned code, and accounted for noneType

--- TestRun.py
+++ TestRun.py
@@ -6,6 +6,19 @@ import csv
 from google.cloud import vision
 from google.cloud.vision import types
 
+def create_labels():
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    image = types.Image(content=content)
+
+    #Performs label detection on the image file
+    response_label = client.label_detection(image=image)
+    labels = response_label.label_annotations
+    document = response_label.full_text_annotation
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
 # Finds input word and input year on page, finds each x and y-values for inputs, and extracts number at the intersection of x y values
 def detect_text(response, path, word, year):
     """Detects text in the file."""
@@ -13,6 +26,7 @@ def detect_text(response, path, word, year):
     texts = response.text_annotations
     print('Texts:')
 
+    #Finds word on page and its y vertices and  prints it as well as its y vertices
     for text in texts:
         if text.description==word:
             print('\n"{}"'.format(text.description))
@@ -22,7 +36,7 @@ def detect_text(response, path, word, year):
     vy = [int((tup.split(","))[1][0:-1]) for tup in vertices1]
     vy = sorted(list(dict.fromkeys(vy)))
 
-
+    #finds year on page and prints it as well as prints its y vertices
     for text in texts:
         if text.description==year:
             print('\n"{}"'.format(text.description))
@@ -32,6 +46,7 @@ def detect_text(response, path, word, year):
     vx = [int((tup.split(","))[0][1:]) for tup in vertices2]
     vx = sorted(list(dict.fromkeys(vx)))
 
+    #find match between x and y vertices and print output
     for text in texts:
         maybe = (['({},{})'.format(vertex.x, vertex.y)
                 for vertex in text.bounding_poly.vertices])
@@ -43,48 +58,7 @@ def detect_text(response, path, word, year):
             if vx[1] in range(mx[0], mx[1]):
                 output = text.description
                 print(output)
-
-    append_csv(word, year, output)
-
-def detect_document(response, path):
-    """Detects document features in an image."""
-
-    for page in response.full_text_annotation.pages:
-        for block in page.blocks:
-            print('\nBlock confidence: {}\n'.format(block.confidence))
-
-            for paragraph in block.paragraphs:
-                print('Paragraph confidence: {}'.format(
-                    paragraph.confidence))
-
-                for word in paragraph.words:
-                    word_text = ''.join([
-                        symbol.text for symbol in word.symbols
-                    ])
-                    print('Word text: {} (confidence: {})'.format(
-                        word_text, word.confidence))
-
-                    for symbol in word.symbols:
-                        print('\tSymbol: {} (confidence: {})'.format(
-                            symbol.text, symbol.confidence))
-
-                    for symbol in word.symbols:
-                        print(symbol.text)
-
-def assemble_word(word):
-    assembled_word=""
-    for symbol in word.symbols:
-        assembled_word+=symbol.text
-    return assembled_word
-
-def find_word_location(document,word_to_find):
-    for page in document.pages:
-        for block in page.blocks:
-            for paragraph in block.paragraphs:
-                for word in paragraph.words:
-                    assembled_word=assemble_word(word_to_find)
-                    if(assembled_word==word_to_find):
-                        return word.bounding_box
+                return output
 
 # Checks all instaces of 'word' on single page and returns and array of True/False based on if they are in a table or not
 def isTable(response, path, word):
@@ -95,6 +69,7 @@ def isTable(response, path, word):
     for text in texts:
         mightBeTable = []
         if text.description==word:
+
             #this part checks back through the whole list of all words to find words with the correct dimensions for their bounding boxes.
             yVertexSet = set()
             xVertexSet = set()
@@ -104,6 +79,7 @@ def isTable(response, path, word):
             rightVertex = max(xVertexSet)
             topVertex = min(yVertexSet)
             bottomVertex = max(yVertexSet)
+
             #til this point, it is setting the bounds for the "correct dimensions"
             for word1 in texts:
                 y2VertexSet = set()
@@ -116,6 +92,7 @@ def isTable(response, path, word):
                 bottom2Vertex = max(y2VertexSet)
                 if right2Vertex > rightVertex and ((topVertex <= top2Vertex and top2Vertex < bottomVertex) or (bottomVertex >= bottom2Vertex and bottom2Vertex >= topVertex)  or ((topVertex >= top2Vertex) and (bottomVertex <= bottom2Vertex))): #checks to see if any vertex of the examined word is in line with the original word, as well as being to the right of the original word
                     mightBeTable.append(word1)
+            
             #this next part is going to check to see if the words that were in the right place are numbers, thus qualifiying if the word is a row in a table
             numberPercent = 0 #tracks how many of the words are numbers
             totalWords = 0 #tracks how many words we examine
@@ -158,23 +135,33 @@ def get_confidence(word):
     return word.confidence
 
 def append_csv(measure, year, value):
-    try:
-        with open("TestRun.csv", "a") as x:
-            x.write(measure + ",")
-            x.write(year + ",")
-            if "," in value:
-                z=value.split(",")
-                y= "".join(z)
-                x.write(y + "\n")
-    except:
-        with open("TestRun.csv", "wt") as x:
-            x.write("Measure,Year,Value\n")
-            x.write(measure + ",")
-            x.write(year + ",")
-            if "," in value:
-                z=value.split(",")
-                y= "".join(z)
-                x.write(y + "\n")
+        try:
+            with open("TestRun.csv", "a") as x:
+                print("daaaaaaaa")
+                x.write(measure + ",")
+                x.write(year + ",")
+                if value:
+                    if "," in value:
+                        z=value.split(",")
+                        y= "".join(z)
+                        x.write(y + "\n")
+                else:
+                    x.write("\n")
+                    print("append_csv: None Type value passed in...")
+        except:
+            with open("TestRun.csv", "wt") as x:
+                print("dzkjbzknb")
+                x.write("Measure,Year,Value\n")
+                x.write(measure + ",")
+                x.write(year + ",")
+                if value:
+                    if "," in value:
+                        z=value.split(",")
+                        y= "".join(z)
+                        x.write(y + "\n")
+                else:
+                    x.write("\n")
+                    print("append_csv: None Type value passed in...")
 
 if __name__=="__main__":
     # Instantiates a client
@@ -182,34 +169,26 @@ if __name__=="__main__":
 
     theFile = 'Amazon_10-K_2018-18.png'
 
-    theyear = theFile.split("-")[1][2:]
+    current_year = theFile.split("-")[1][2:]
 
     # The name of the image file to annotate
     file_name = os.path.join(
         os.path.dirname(__file__),
         theFile)
 
-    # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
         content = image_file.read()
     image = types.Image(content=content)
-
-    # Performs label detection on the image file
-    #response_label = client.label_detection(image=image)
-    #labels = response_label.label_annotations
-    #document = response_label.full_text_annotation
-    #location=find_word_location(document,"for")
-    #print(location)
-
-    #print('Labels:')
-    #for label in labels:
-    #    print(label.description)
 
     response_doc = client.document_text_detection(image=image)
     response = client.text_detection(image=image)
 
     #detect_document(response_doc, file_name)
 
-    detect_text(response, file_name, "sales", "2018")
+    word = "income"
+
+    create_labels()
+    output = detect_text(response, file_name, word, "2015")
+    append_csv(word, current_year, output)
 
     print(isTable(response, file_name, "earnings"))
