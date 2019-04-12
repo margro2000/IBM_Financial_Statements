3f28a649398efda14db1420d0ea6b205029fd579
Omar Chbaklo <chbakloomar@gmail.com>
2019-04-12 04:30:41 -0400

    Implemented loop

--- TestRun.py
+++ TestRun.py
@@ -45,38 +45,43 @@ def detect_text(response, path, word, year):
 
     texts = response.text_annotations
     doc_texts = response.full_text_annotation
+    #count = -1
     print('Texts:')
 
     #Finds word on page and its y vertices and  prints it as well as its y vertices
-    #try:
-    for text in texts:
-        if text.description==word:
-            print('\n"{}"'.format(text.description))
-            vertices1 = ({'({},{})'.format(vertex.x, vertex.y)
-                for vertex in text.bounding_poly.vertices})
-            print('bounds: {}'.format(','.join(vertices1)))
-    vy = [int((tup.split(","))[1][0:-1]) for tup in vertices1]
-    vy = sorted(list(dict.fromkeys(vy)))
+    try:
+        for text in texts:
+            if text.description==word:
+                #count = count + 1
+                #if isTable(response, path, word)[count]:
+                print('\n"{}"'.format(text.description))
+                vertices1 = ({'({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices})
+                print('bounds: {}'.format(','.join(vertices1)))
+        vy = [int((tup.split(","))[1][0:-1]) for tup in vertices1]
+        vy = sorted(list(dict.fromkeys(vy)))
+    except:
+        print("Error extracting word \"" + word + "\"")
 
-   # except:
-        #print("Error extracting word")
+    #Finds year on page and prints it as well as prints its y vertices
+    try:
+        for text in texts:
+            #this willl capture the most recent instance of the year
+            if text.description==year:
+                print('\n"{}"'.format(text.description))
+                vertices2 = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+                print('bounds: {}'.format(','.join(vertices2)))
+        vx = [int((tup.split(","))[0][1:]) for tup in vertices2]
+        vx = sorted(list(dict.fromkeys(vx)))
+    except:
+        print("Error extracting year \"" + year + "\"")
 
-    #try:
-        #finds year on page and prints it as well as prints its y vertices
-    for text in texts:
-        #this willl capture the most recent instance of the year
-        if text.description==year:
-            print('\n"{}"'.format(text.description))
-            vertices2 = (['({},{})'.format(vertex.x, vertex.y)
-                for vertex in text.bounding_poly.vertices])
-            print('bounds: {}'.format(','.join(vertices2)))
-    vx = [int((tup.split(","))[0][1:]) for tup in vertices2]
-    vx = sorted(list(dict.fromkeys(vx)))
-
-    output = ""
-    confidence = 0
-    print("VX" + str(vx))
-    for page in doc_texts.pages:
+    try:
+        output = ""
+        confidence = 0
+        print("VX" + str(vx))
+        for page in doc_texts.pages:
             for block in page.blocks:
                 for paragraph in block.paragraphs:
                     for word_obj in paragraph.words:
@@ -84,8 +89,8 @@ def detect_text(response, path, word, year):
                         word_text = ''.join([
                             symbol.text for symbol in word_obj.symbols
                         ])
-                        mx =set()
-                        my =set()
+                        mx = set()
+                        my = set()
                         for vertex in word_obj.bounding_box.vertices:
                             mx.add(vertex.x)
                             my.add(vertex.y)
@@ -98,10 +103,13 @@ def detect_text(response, path, word, year):
                             if mx[0] <= vx[1] and vx[1] <= mx[1]:
                                 output = word_text
                                 confidence = get_confidence(word_obj)
+    except:
+        print("Error extracting intersection of word \"" + word + "\" and year \"" + year + "\"")
 
-    print(output)
-    print(confidence)
-    append_csv(word, year, output, confidence)
+    #print(output)
+    #print(confidence)
+    #append_csv(word, year, output, confidence)
+    return (output, confidence)
 
 def detect_document(response, path):
     """Detects document features in an image."""
@@ -237,17 +245,17 @@ def pdfToPng(path, popplerPath):
     #         newfilename = f[:-4] + str(n) + '.png'
     #         Image(images[i]).save(filename=newfilename)
 
-def append_csv(measure, year, value, confidence):
+def append_csv(measure, year, value, confidence, company):
     try:
-        with open("TestRun.csv", "r") as x:
+        with open(company + ".csv", "r") as x:
             pass
     except:
-        with open("TestRun.csv", "wt") as x:
+        with open(company + ".csv", "wt") as x:
             x.write("Measure,Year,Value,Confidence\n")
     
-    with open("TestRun.csv", "a") as x:
-        x.write(measure + ",")
-        x.write(year + ",")
+    with open(company + ".csv", "a") as x:
+        x.write(str(measure) + ",")
+        x.write(str(year) + ",")
         if "," in value:
             z=value.split(",")
             y= "".join(z)
@@ -258,36 +266,34 @@ if __name__=="__main__":
     # Instantiates a client
     client = vision.ImageAnnotatorClient()
 
-    theFile = 'Amazon_10-K_2018-18.png'
-    #theFile = pdfToPng("Alphabet_10K_2017.pdf")
-
-    current_year = theFile.split("-")[1][2:]
+    thePath = 'Amazon_10-K_2018'
+    word = "income"
+    
+    current_year = int(thePath.split("_")[2], 10)
+    final_year = current_year - 2
 
     # The name of the image file to annotate
-    file_name = os.path.join(
+    file_path = os.path.join(
         os.path.dirname(__file__),
-        theFile)
+        thePath)
 
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
-    image = types.Image(content=content)
+    for file_name in os.listdir(file_path):
+        theCompany = file_name.split("_")[0]
+        with io.open(os.path.join(file_path,file_name), 'rb') as image_file:
+            content = image_file.read()
+        image = types.Image(content=content)
 
-    response_doc = client.document_text_detection(image=image)
-    response = client.text_detection(image=image)
-    word = "sales"
+        response_doc = client.document_text_detection(image=image)
+        response = client.text_detection(image=image)
 
-    #detect_document(response_doc, file_name)
-
-    #detect_text(response, file_name, "sales", "2018")
+        cur_year = current_year
+        while cur_year >= final_year:
+            (output, confidence) = detect_text(response, file_name, word, str(cur_year))
+            append_csv(word, cur_year, output, confidence, theCompany)
+            cur_year = cur_year - 1
     
-    current_year = int(current_year,10)
-    final_year = current_year - 4
-    while current_year >= final_year:
-        output = detect_text(response, file_name, word, str(current_year))
-        current_year = current_year - 1
-
+    #detect_document(response_doc, file_name)
+    #detect_text(response, file_name, "sales", "2018")
     #printAllText(response, file_name)
-
     #create_labels()
-
     #print(isTable(response, file_name, "earnings"))
--- regexTest.py
+++
@@ -1,2 +0,0 @@
-import re
-print(re.match(r'\(?\d+[.,0-9]*\)?', "123,123"))
